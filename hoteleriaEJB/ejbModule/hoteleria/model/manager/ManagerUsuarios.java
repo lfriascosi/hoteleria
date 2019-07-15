98569e28435040710f5258ac5980253ab303c90c
GisselaMIreya <gissemireyitaq@gmail.com>
2019-07-15 11:23:50 -0500

    Insertar usuarios

---
+++ hoteleriaEJB/ejbModule/hoteleria/model/manager/ManagerUsuarios.java
@@ -0,0 +1,58 @@
+package hoteleria.model.manager;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import hoteleria.model.entities.InvUsuario;
+
+/**
+ * Session Bean implementation class ManagerUsuarios
+ */
+@Stateless
+@LocalBean
+public class ManagerUsuarios {
+	@PersistenceContext
+	private EntityManager em;
+	
+    public ManagerUsuarios() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public List<InvUsuario> findAllInvUsuarios(){
+    	String consulta="select u from InvUsuario u";
+    	Query q=em.createQuery(consulta, InvUsuario.class);
+    	return q.getResultList();
+    }
+    
+    public InvUsuario findInvUsuarioById(int idusuario) {
+    	return em.find(InvUsuario.class, idusuario);
+    }
+    
+    public InvUsuario insertarInvUsuario(InvUsuario usuario) {
+    	InvUsuario usuarios = new InvUsuario();
+    	usuarios.setApellidosusuario(usuario.getApellidosusuario());
+    	usuarios.setNombresusuario(usuario.getNombresusuario());
+    	usuarios.setClave(usuario.getClave());
+    	usuarios.setCorreo(usuario.getCorreo());
+    	usuarios.setDireccion(usuario.getDireccion());
+    	usuarios.setEstado(usuario.getEstado());
+    	usuarios.setTelefono(usuario.getTelefono());
+    	usuarios.setFechacreacion(usuario.getFechacreacion());
+    	usuarios.setFechaactualizacion(usuario.getFechaactualizacion());
+    	em.persist(usuarios);
+    	return usuarios;
+    }
+    
+    public void EliminarInvUsuario (Integer idusuario) {
+    	InvUsuario usuario = findInvUsuarioById(idusuario);
+    	if(usuario!=null)
+    		em.remove(usuario);
+    }
+    
+
+}
---
+++ hoteleriaWeb/src/hoteleria/controller/BeanUsuarios.java
@@ -0,0 +1,92 @@
+package hoteleria.controller;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import habitaciones.controller.JSFUtil;
+import hoteleria.model.entities.InvTiposhabitacione;
+import hoteleria.model.entities.InvUsuario;
+import hoteleria.model.manager.ManagerUsuarios;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class BeanUsuarios implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private ManagerUsuarios managerUsuarios;
+	private List<InvUsuario> listaInvUsuarios;
+	private InvUsuario usuario;
+	private boolean panelColapsado;
+	private InvUsuario usuarioSeleccionada;
+
+	@PostConstruct
+	public void inicializar() {
+		listaInvUsuarios = managerUsuarios.findAllInvUsuarios();
+		usuario = new InvUsuario();
+		panelColapsado = true;
+	}
+
+	public void actionListenerCOlapsarPanel() {
+		panelColapsado = !panelColapsado;
+	}
+
+	public void actionListenerInsertarUsuario() {
+		try {
+			managerUsuarios.insertarInvUsuario(usuario);
+			listaInvUsuarios = managerUsuarios.findAllInvUsuarios();
+			usuario = new InvUsuario();
+			JSFUtil.crearMensajeInfo("Datos de usuario insertados.");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public void actionListenerELiminarUsuario(Integer idusuario) {
+		managerUsuarios.EliminarInvUsuario(idusuario);
+		listaInvUsuarios = managerUsuarios.findAllInvUsuarios();
+		JSFUtil.crearMensajeInfo("Usuario eliminado");
+	}
+	
+	public void actionListenerSelecionarUsuarios(InvUsuario usuario) {
+		usuarioSeleccionada = usuario;
+	}
+
+	public List<InvUsuario> getListaInvUsuarios() {
+		return listaInvUsuarios;
+	}
+
+	public void setListaInvUsuarios(List<InvUsuario> listaInvUsuarios) {
+		this.listaInvUsuarios = listaInvUsuarios;
+	}
+
+	public InvUsuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(InvUsuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public boolean isPanelColapsado() {
+		return panelColapsado;
+	}
+
+	public void setPanelColapsado(boolean panelColapsado) {
+		this.panelColapsado = panelColapsado;
+	}
+
+	public InvUsuario getUsuarioSeleccionada() {
+		return usuarioSeleccionada;
+	}
+
+	public void setUsuarioSeleccionada(InvUsuario usuarioSeleccionada) {
+		this.usuarioSeleccionada = usuarioSeleccionada;
+	}
+
+}
